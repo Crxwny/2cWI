71a352863864f7e501374855efc26a60c0a5a254
Damian <damian.thoma@student.htldornbirn.at>
2023-10-12 09:03:19 +0200

    JS_01_Variablen_Operationen_finished

---
+++ JS/JS_01_Variablen_Operationen/script.js
@@ -0,0 +1,51 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+
+console.log("a = " +a)
+console.log("b = " +b)
+console.log("c = " +c)
+
+// Berechne folgendes und gib das Eregebnis über console.log aus
+// a + b + c
+let d = a + b + c;
+console.log("a + b + c = " + d);
+
+// (a + b) / c
+d = (a + b) / c;
+console.log("(a + b) / c = " + d);
+
+// c -a + b
+d = c - a + b;
+console.log("c - a + b = " + d);
+
+// a + b + c * 3
+d = a + b + c * 3;
+console.log("a + b + c * 3 = " + d);
+
+// (a + b + c)/4
+d = (a + b + c) / 4;
+console.log("(a + b + c) / 4 = " + d);
+
+// Ändere die Variablen auf folgende Werte
+// a = 3
+a = 3
+
+// b = a + 3
+b= a+3
+
+// c = c -3
+c = c-3
+
+// gib die neuen Werte der Variablen aus
+console.log("Neue Werte für a, b, c:")
+console.log("a = " +a)
+console.log("b = " +b)
+console.log("c = " +c)
+
+// Rechnungsarten
+// *; /; +; -;
