efb42bedc9c1cb8edbaa1b03634de20539efe22f
Damian <damian.thoma@student.htldornbirn.at>
2023-10-12 09:30:18 +0200

    EndOfLesson

---
+++ JS/JS_03_Uebung_Variablen/script.js
@@ -0,0 +1,17 @@
+let banana = "Banane";
+let apple = "Apple";
+
+let bananaPricePerKilo = 2.14;
+let applePricePerKilo = 3.43;
+
+let appleWeight = 0.34;
+let bananaWeight = 0.22;
+
+let amountOfBananas = 17;
+let amountOfApples = 8;
+
+let applePerKilo = 1 / 0.34
+console.log("Anzahl von Bananen: " +amountOfBananas);
+console.log("Äpfel pro Kilo: " +applePerKilo)
+
+
