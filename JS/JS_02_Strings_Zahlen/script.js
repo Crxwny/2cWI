18ada51a74b59411bb2134cd668288dc2e6950af
Damian <damian.thoma@student.htldornbirn.at>
2023-10-12 09:18:10 +0200

    JS_02_Strings_Zahlen_finished

---
+++ JS/JS_02_Strings_Zahlen/script.js
@@ -0,0 +1,10 @@
+let a = 17.4;
+let b = 3.9;
+let result = a / b;
+
+console.log("The result of " + a + " divided by " + b + " is " + result);
+
+let firstname = "Damian";
+let lastname = "Thoma";
+
+console.log("Ich heiße " + firstname+ " "  +lastname);
