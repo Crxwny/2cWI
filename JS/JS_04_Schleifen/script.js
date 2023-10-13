0caaa314c0d593abb84f8ca09efad4b743562464
Crxwny <damian.thoma@student.htldornbirn.at>
2023-10-13 19:49:00 +0200

    JS_04_Schleifen_finished

---
+++ JS/JS_04_Schleifen/script.js
@@ -0,0 +1,47 @@
+// Zähle von 7 bis 45
+for (let count = 7; count <= 45; count++) {
+  console.log(count);
+}
+
+console.log("Pause");
+
+// Zähle von 9 bis 99
+for (let count2 = 9; count2 <= 99; count2++) {
+  console.log(count2);
+}
+
+console.log("Pause");
+
+// Zähle rückwärts von 10 bis 0
+for (let count3 = 10; count3 >= 0; count3--) {
+  console.log(count3);
+}
+
+console.log("Pause");
+
+// Zähle rückwärts von 1234 bis 1207
+for (let count4 = 1234; count4 >= 1207; count4--) {
+  console.log(count4);
+}
+
+console.log("Pause");
+
+// Gib Deinen Namen genau 8 mal aus
+for (let countname = 1; countname <= 8; countname++) {
+  console.log("Damian");
+}
+
+console.log("Pause");
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle von 10 auf 0
+for (let count5 = 0; count5 <= 10; count5++) {
+  console.log(count5);
+}
+
+console.log("Damian");
+
+for (let count6 = 10; count6 >= 0; count6--) {
+  console.log(count6);
+}
+
+console.log("Pause");
