027727402584e49eaf654256e6f3d5ab9aea6fbd
Crxwny <damian.thoma@student.htldornbirn.at>
2023-10-17 19:31:39 +0200

    JS_05_If_finished

---
+++ JS/JS_05_If/script.js
@@ -0,0 +1,29 @@
+// Task 1
+let number1 = 51;
+
+if (number1 < 20) {
+  console.log("Mini");
+}
+
+if (number1 >= 20 && number1 <= 50) {
+  console.log("Medium");
+}
+
+if (number1 > 50) {
+  console.log("Large");
+}
+
+// Task 2
+let number2 = 8;
+
+if (number2 <= 7) {
+  console.log("Mini");
+}
+
+if (number2 > 7 && number2 <= 90) {
+  console.log("Medium");
+}
+
+if (number2 > 90) {
+  console.log("Large");
+}
