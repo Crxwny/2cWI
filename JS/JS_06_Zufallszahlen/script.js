09fe6595d720c4b38c2072e073219c87e0a52ecb
Damian <damian.thoma@student.htldornbirn.at>
2023-10-19 08:15:50 +0200

    JS_06_Zufallszahlen_finished

---
+++ JS/JS_06_Zufallszahlen/script.js
@@ -0,0 +1,6 @@
+let randomNumber = Math.random() * 10 + 10; // Math.random gibt Zahl von 0 - 1 aus, *10 für Zahlen von 1 -10 und +10 für Zahlen von 1-10 + 10 also Zahlen von 10-20
+let random = Math.floor(randomNumber);
+
+console.log(random);
+
+// 1 2 3 4 5 6 7 8 9 10 1 12 ... 19 20
