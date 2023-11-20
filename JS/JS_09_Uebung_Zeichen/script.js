ccb1e7509fe673944784e17eb4221a5e0b6e28cd
Crxwny <damian.thoma@student.htldornbirn.at>
2023-11-20 22:42:35 +0100

    JS_09_Uebung_Zeichen_task2_finished

--- JS/JS_09_Uebung_Zeichen/script.js
+++ JS/JS_09_Uebung_Zeichen/script.js
@@ -1,17 +1,26 @@
 // Task 1
 for (let index = 0; index < 4; index++) {
-    if (index%2==0) {
-        console.log("XXXX");
-        
-    } if (index%2!=0) {
-        console.log("OOOO");
-    } 
+  if (index % 2 == 0) {
+    console.log("XXXX");
+  }
+  if (index % 2 != 0) {
+    console.log("OOOO");
+  }
 }
 
 // Task 2
-for (let index = 1; index < 100; index++) {
-    if (index%2==0) {
-        console.log(index + index);
-    }
-    
+let sum = 0;
+
+for (let nejo = 1; nejo <= 100; nejo++) {
+  if (nejo % 2 === 0) {
+    sum += nejo;
+  }
 }
+
+console.log(sum);
+
+// Task 3
+
+
+
+
