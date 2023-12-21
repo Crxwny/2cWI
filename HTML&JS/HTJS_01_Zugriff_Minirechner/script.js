9d92cfa9f517596ed11c314c6bdee41e92436711
Damian <damian.thoma@student.htldornbirn.at>
2023-12-21 09:17:05 +0100

    end_of_lesson

---
+++ HTML&JS/HTJS_01_Zugriff_Minirechner/script.js
@@ -0,0 +1,59 @@
+function Add() {
+  console.log("Added");
+  let num1 = document.getElementById("number1").value;
+  let num2 = document.getElementById("number2").value;
+
+
+  let result = parseFloat(num1) + parseFloat(num2);
+  if (result>= 0 || result <= 0 ) {
+    console.log(result);
+
+  } else {
+    console.log("Invalid Input, try again");
+  }
+}
+
+function Subtract() {
+  console.log("Subtraced");
+  let num1 = document.getElementById("number1").value;
+  let num2 = document.getElementById("number2").value;
+
+
+  let result = parseFloat(num1) - parseFloat(num2);
+  if (result>= 0 || result <= 0 ) {
+    console.log(result);
+
+  } else {
+    console.log("Invalid Input, try again");
+  }
+}
+
+function Multiply() {
+  console.log("Multiplied");
+  let num1 = document.getElementById("number1").value;
+  let num2 = document.getElementById("number2").value;
+
+
+  let result = parseFloat(num1) * parseFloat(num2);
+  if (result>= 0 || result <= 0 ) {
+    console.log(result);
+
+  } else {
+    console.log("Invalid Input, try again");
+  }
+}
+
+function Divide() {
+  console.log("Divided");
+  let num1 = document.getElementById("number1").value;
+  let num2 = document.getElementById("number2").value;
+
+
+  let result = parseFloat(num1) / parseFloat(num2);
+  if (result>= 0 || result <= 0 ) {
+    console.log(result);
+
+  } else {
+    console.log("Invalid Input, try again");
+  }
+}
