395fdf48e56eca9344b46a06dda40811d115f61d
Crxwny <damian.thoma@student.htldornbirn.at>
2023-12-21 18:24:58 +0100

    HTJS_01_finished

--- HTML&JS/HTJS_01_Zugriff_Minirechner/script.js
+++ HTML&JS/HTJS_01_Zugriff_Minirechner/script.js
@@ -2,58 +2,54 @@ function Add() {
   console.log("Added");
   let num1 = document.getElementById("number1").value;
   let num2 = document.getElementById("number2").value;
-
-
+ 
   let result = parseFloat(num1) + parseFloat(num2);
-  if (result>= 0 || result <= 0 ) {
-    console.log(result);
-
+  if (result >= 0 || result <= 0) {
+     updateResult(result);
   } else {
-    console.log("Invalid Input, try again");
+     console.log("Invalid Input, try again");
   }
-}
-
-function Subtract() {
+ }
+ 
+ function Subtract() {
   console.log("Subtraced");
   let num1 = document.getElementById("number1").value;
   let num2 = document.getElementById("number2").value;
-
-
+ 
   let result = parseFloat(num1) - parseFloat(num2);
-  if (result>= 0 || result <= 0 ) {
-    console.log(result);
-
+  if (result >= 0 || result <= 0) {
+     updateResult(result);
   } else {
-    console.log("Invalid Input, try again");
+     console.log("Invalid Input, try again");
   }
-}
-
-function Multiply() {
+ }
+ 
+ function Multiply() {
   console.log("Multiplied");
   let num1 = document.getElementById("number1").value;
   let num2 = document.getElementById("number2").value;
-
-
+ 
   let result = parseFloat(num1) * parseFloat(num2);
-  if (result>= 0 || result <= 0 ) {
-    console.log(result);
-
+  if (result >= 0 || result <= 0) {
+     updateResult(result);
   } else {
-    console.log("Invalid Input, try again");
+     console.log("Invalid Input, try again");
   }
-}
-
-function Divide() {
+ }
+ 
+ function Divide() {
   console.log("Divided");
   let num1 = document.getElementById("number1").value;
   let num2 = document.getElementById("number2").value;
-
-
+ 
   let result = parseFloat(num1) / parseFloat(num2);
-  if (result>= 0 || result <= 0 ) {
-    console.log(result);
-
+  if (result >= 0 || result <= 0) {
+     updateResult(result);
   } else {
-    console.log("Invalid Input, try again");
+     console.log("Invalid Input, try again");
   }
-}
+ }
+ 
+ function updateResult(value) {
+  document.getElementById("result").textContent = value;
+ }
