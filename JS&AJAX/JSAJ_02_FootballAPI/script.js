d02c74d857375d87023850e71eeba0f9d1796228
Damian <damian.thoma@student.htldornbirn.at>
2024-02-15 09:05:52 +0100

    JSAJ_02_FootballAPI_finished

--- JS&AJAX/JSAJ_02_FootballAPI/script.js
+++ JS&AJAX/JSAJ_02_FootballAPI/script.js
@@ -1,8 +1,20 @@
 function loadData(params) {
   fetch("https://api.openligadb.de/getbltable/bl1/2022").then((result) => {
     result.json().then((data) => {
-    console.log(data);})
+      console.log(data);
+      fillTable(data);
+    });
   });
-}
+};
+
+const fillTable = (data) => {
+  let html = "";
+
+  data.forEach((element) => {
+    html +=
+      "<div>" + element.points + " by Team " + element.teamName + "</div>";
+  });
+  document.getElementById("output").innerHTML = html;
+};
 
 loadData();
